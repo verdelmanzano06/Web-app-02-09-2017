64c48adf6239cb3d7a8389394da235d4a575a649
verdelmanzano06 <verdelmanzano06@users.noreply.github.com>
2017-02-09 12:22:16 +0800

    Update anyco.php

--- anyco.php
+++ anyco.php
@@ -1,6 +1,25 @@
 <?php
   require("anyco_ui.inc");
-  
+  $conn = oci_connect('hr','hr', '//localhost/XE');
   ui_print_header('Departments');
+  do_query($conn, 'SELECT * FROM DEPARTMENTS');
   ui_print_footer(date('Y-m-d H:i:s'));
+
+  function do_query($conn, $query) {
+    $stid = oci_parse($conn, $query);
+    $r = oci_execute($stid, OCI_DEFAULT);
+    print '<table border="1">';
+    
+    while ($row = oci_fetch_array($stid, OCI_ASSOC + OCI_RETURN_NULLS))
+    {
+      print '<tr>';
+      foreach ($row as $item) {
+        print '<td>';
+          ($item ? htmlentities($item): '&nbsp;'). '</td>';
+      }
+      print '</tr>';
+    }
+    print '</table>'>
+}
+                    
 ?>
